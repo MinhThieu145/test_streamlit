a9c0916678f58be0754403a1457bedcf8b057e39
MinhThieu145 <88282475+MinhThieu145@users.noreply.github.com>
2022-10-29 22:24:57 -0400

    Update main.py

--- USF library/main.py
+++ USF library/main.py
@@ -11,17 +11,4 @@ import plotly.express as px
 
 st.title("Welcome to my world")
 st.write("### Sucks. Isn't it?")
-    
-# import df
-df = pd.read_csv('library_schedule_10_29.csv')
 
-st.write(df)
-x_axis_value = st.selectbox('Select Value', options=df.columns)
-
-graph = px.histogram(data_frame=df, x =x_axis_value, text_auto=True)
-st.plotly_chart(graph)
-
-x_axis_value = st.selectbox('Select X Value', options=df.columns)
-
-graph = px.histogram(data_frame=df, x =x_axis_value, text_auto=True)
-st.plotly_chart(graph)
