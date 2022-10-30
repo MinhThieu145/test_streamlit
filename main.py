a27a5b07c0f42605de215438490e3c589db65526
MinhThieu145 <88282475+MinhThieu145@users.noreply.github.com>
2022-10-29 23:16:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,3 @@ import streamlit as st
 st.title("Welcome to my world")
 st.write("### Sucks. Isn't it?")
     
-# import df
-df = pd.read_csv('library_schedule_10_29.csv')
-
-st.write(df)
-x_axis_value = st.selectbox('Select Value', options=df.columns)
-
-graph = px.histogram(data_frame=df, x =x_axis_value, text_auto=True)
-st.plotly_chart(graph)
-
-x_axis_value = st.selectbox('Select X Value', options=df.columns)
-
-graph = px.histogram(data_frame=df, x =x_axis_value, text_auto=True)
-st.plotly_chart(graph)
