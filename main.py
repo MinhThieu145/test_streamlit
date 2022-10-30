79733f8b6394928faaedc3fce93261244bce5dcb
MinhThieu145 <88282475+MinhThieu145@users.noreply.github.com>
2022-10-29 22:34:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import sklearn
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-import chart_studio.plotly as py
-import plotly.express as px
 
 
 st.title("Welcome to my world")
