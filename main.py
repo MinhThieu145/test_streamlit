13bb51418e9c7c244044da0a66d0831dc3570b71
MinhThieu145 <88282475+MinhThieu145@users.noreply.github.com>
2022-10-29 23:31:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import s3fs
+import os
 
 
 st.title("Welcome to my world")
 st.write("### Sucks. Isn't it?")
     
+fs = s3fs.S3FileSystem(anon=False)
+
+# Retrieve file contents.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def read_file(filename):
+    with fs.open(filename) as f:
+        return f.read().decode("utf-8")
+
+content = read_file("library-scraping-storage/library_schedule_10_29.csv")
+
+# Print results.
+for line in content.strip().split("\n"):
+    st.write(line)
